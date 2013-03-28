db975f9b4cb240792a77082c66d84787b2dfeec9
Jeremy Skogen <jeremyskogen@gmail.com>
2013-03-28 16:55:06 -0700

    Stuff

---
+++ sdcc/bingo.js
@@ -0,0 +1,268 @@
+// http://blo0p.deviantart.com/gallery/32499734
+
+var tiles = [
+	{
+		id : 0,
+		name : "Free Spot",
+		img_src : "http://2.bp.blogspot.com/-rrXH9umMEeM/TmTAqSoXaVI/AAAAAAAABNU/MGttPe2x9V4/s1600/frr+flower.png"
+	},{
+		id : 1,
+		name : "The Tick",
+		img_src : "http://weddingphotography.com.ph/wp-content/uploads/2011/11/2-might-blue-justice-150x150.jpg"	
+	},
+	{
+		id : 2,
+		name : "Magneto",
+		img_src : "http://weddingphotography.com.ph/wp-content/uploads/2011/11/3-magnus-150x150.jpg"
+	},
+	{
+		id : 3,
+		name : "The Joker",
+		img_src : "http://weddingphotography.com.ph/wp-content/uploads/2011/11/4-clown-prince-of-crime-150x150.jpg"
+	},
+	{
+		id : 4,
+		name : "Professor Doom",
+		img_src : "http://weddingphotography.com.ph/wp-content/uploads/2011/11/5-the-master-150x150.jpg"
+	},
+	{
+		id : 5,
+		name : "The Green Goblin",
+		img_src : "http://weddingphotography.com.ph/wp-content/uploads/2011/11/9-the-goblin-lord-150x150.jpg"
+	},{
+		id : 6,
+		name : "Storm",
+		img_src : "http://weddingphotography.com.ph/wp-content/uploads/2011/11/15-goddess-of-the-plains-150x150.jpg"
+	},{
+		id : 7,
+		name : "Jean Grey",
+		img_src : "http://weddingphotography.com.ph/wp-content/uploads/2011/11/20-ms-psyche-150x150.jpg"
+	},{
+		id : 8,
+		name : "Wolverine",
+		img_src : "http://weddingphotography.com.ph/wp-content/uploads/2011/11/13-weapon-x-150x150.jpg"
+	},
+	{
+		id : 9,
+		name : "Cyclops",
+		img_src : "http://weddingphotography.com.ph/wp-content/uploads/2011/11/14-slym-dayspring-150x150.jpg"
+	},
+	{
+		id : 10,
+		name : "Rogue",
+		img_src : "http://weddingphotography.com.ph/wp-content/uploads/2011/11/19-anna-raven-150x150.jpg"
+	},{
+		id : 11,
+		name : "Captain America",
+		img_src : "http://weddingphotography.com.ph/wp-content/uploads/2011/11/26-the-great-defender-150x150.jpg"
+	},{
+		id : 12,
+		name : "Iron Man",
+		img_src : "http://weddingphotography.com.ph/wp-content/uploads/2011/11/27-the-armored-avenger-150x150.jpg"
+	},{
+		id : 13,
+		name : "Thor",
+		img_src : "http://weddingphotography.com.ph/wp-content/uploads/2011/11/28-son-of-odin-150x150.jpg"
+	},{
+		id : 14,
+		name : "Spiderman",
+		img_src : "http://weddingphotography.com.ph/wp-content/uploads/2011/11/29-the-web-slinger-150x150.jpg"
+	},{
+		id : 15,
+		name : "Hawkeye",
+		img_src : "http://weddingphotography.com.ph/wp-content/uploads/2011/11/33-the-avenging-archer-150x150.jpg"
+	},{
+		id : 16,
+		name : "Batman",
+		img_src : "http://weddingphotography.com.ph/wp-content/uploads/2011/11/35-caped-crusader-150x150.jpg"
+	},{
+		id : 17,
+		name : "Batgirl",
+		img_src : "http://th04.deviantart.net/fs16/150/i/2007/221/3/c/Batgirl_by_VietNguyen.jpg"
+		// "http://fc08.deviantart.net/fs16/i/2007/221/3/c/Batgirl_by_VietNguyen.jpg"
+	},{
+		id : 18,
+		name : "Catwoman",
+		img_src : "http://twistedsifter.sifter.netdna-cdn.com/wp-content/uploads/2012/02/superheroes-and-villains-minimal-art-posters-by-bloop-7.jpg"
+	},{
+		id : 19,
+		name : "Wonder Woman",
+		img_src : "http://weddingphotography.com.ph/wp-content/uploads/2011/11/36-amazon-princess-150x150.jpg"
+	},{
+		id : 20,
+		name : "Green Lantern",
+		img_src : "http://weddingphotography.com.ph/wp-content/uploads/2011/11/37-the-emerald-crusader-150x150.jpg"
+	},{
+		id : 21,
+		name : "Superman",
+		img_src : "http://weddingphotography.com.ph/wp-content/uploads/2011/11/39-the-man-of-steel-150x150.jpg"
+	},{
+		id : 22,
+		name : "Supergirl",
+		img_src : "http://th08.deviantart.net/fs70/150/f/2010/341/b/5/supergirl_by_miketheartist-d34edv5.jpg"
+	},{
+		id : 23,
+		name : "Rorshank",
+		img_src : "http://th08.deviantart.net/fs70/150/i/2012/020/3/c/kovacs_by_blo0p-d4n02x4.jpg"
+	},{
+		id : 24,
+		name : "Powergirl",
+		img_src : "http://th07.deviantart.net/fs70/150/f/2011/137/4/8/power_girl_27_by_warrenlouw-d3gjjia.jpg"
+	},{
+		id : 25,
+		name : "Ash Ketchum",
+		img_src : "http://ih3.redbubble.net/image.11810143.7777/flat,135x135,075,t.jpg"
+	},{
+		id : 26,
+		name : "Misty or Staru",
+		img_src : "http://th07.deviantart.net/fs71/150/i/2010/188/4/b/Misty_and_Staryu_by_Windnstorm.jpg"
+	},{
+		id : 27,
+		name : "Brock",
+		img_src : "http://th05.deviantart.net/fs5/150/i/2004/297/6/6/Brock_by_arisceima.jpg"
+	},{
+		id : 28,
+		name : "Lollipop Chainsaw",
+		img_src : "http://th04.deviantart.net/fs71/150/f/2012/141/f/d/lollipop_chainsaw_by_guilhermepato-d50nbyc.jpg"
+	},{
+		id : 29,
+		name : "Vampire",
+		img_src : "http://th01.deviantart.net/fs27/150/f/2008/069/5/2/52c26c1a21d1b574.jpg"
+	},{
+		id : 30,
+		name : "Boba Fett",
+		img_src : "http://editorial.designtaxi.com/news-starwars2203/8.jpg"
+	},{
+		id : 31,
+		name : "Darth Vader",
+		img_src : "http://editorial.designtaxi.com/news-starwars2203/9.jpg"
+	},{
+		id : 32,
+		name : "Rebel",
+		img_src : "http://editorial.designtaxi.com/news-starwars2203/7.jpg"
+	},{
+		id : 33,
+		name : "Regular Leia",
+		img_src : "http://editorial.designtaxi.com/news-starwars2203/5.jpg"
+	},{
+		id : 34,
+		name : "Slave Leia",
+		img_src : "http://th02.deviantart.net/fs71/150/f/2010/052/5/a/Slave_leia_by_ArtbyBones.jpg"
+	},{
+		id : 35,
+		name : "R2D2",
+		img_src : "http://editorial.designtaxi.com/news-starwars2203/2.jpg"
+	},{
+		id : 36,
+		name : "Stormtrooper",
+		img_src : "http://editorial.designtaxi.com/news-starwars2203/1.jpg"
+	},{
+		id : 37,
+		name : "Han Solo",
+		img_src : "http://th01.deviantart.net/fs70/150/f/2012/099/a/d/ad72a0b8509f1255856d62652b8424c4-d4vkpyh.png"
+	},{
+		id : 38,
+		name : "Luke Skywalker",
+		img_src : "http://th05.deviantart.net/fs31/150/f/2008/193/e/f/Luke_by_AdamHughes.jpg"
+	},{
+		id : 39,
+		name : "Yoda",
+		img_src : "http://th06.deviantart.net/fs70/150/f/2010/160/8/b/YODA_by_el_grimlock.jpg"
+	},{
+		id : 40,
+		name : "Samus",
+		img_src : "http://th03.deviantart.net/fs51/150/f/2009/311/8/8/Sketch_Card___Samus_commission_by_gb2k.jpg"
+	},{
+		id : 41,
+		name : "Mario or Luigi",
+		img_src : "http://th04.deviantart.net/fs70/150/i/2010/044/e/8/The_Mario_Brothers_by_ry_spirit.jpg"
+	},{
+		id : 42,
+		name : "Peach or Daisy",
+		img_src : "http://th02.deviantart.net/fs6/150/i/2005/089/c/b/Princes_Peach_n_Princess_Daisy_by_usako_chan.jpg"
+	},{
+		id : 43,
+		name : "Kickass",
+		img_src : "http://th06.deviantart.net/fs71/150/f/2012/109/1/9/19a3ed5c824dd4c01f71fa83f42d8d78-d4wuheg.jpg"
+	},{
+		id : 44,
+		name : "Hit Girl",
+		img_src : "http://th07.deviantart.net/fs70/150/f/2010/254/0/9/hit_girl_by_joeyvegasics-d2yhtyv.jpg"
+	},{
+		id : 45,
+		name : "Ramona Flowers",
+		img_src : "http://th04.deviantart.net/fs70/150/i/2011/199/a/c/character_select__ramona_by_tloessy-d40llqu.jpg"
+	},{
+		id : 46,
+		name : "Girl Zombie",
+		img_src : "http://th06.deviantart.net/fs70/150/f/2010/296/3/2/zombie_nurse_3_by_fasslayer-d31dbtu.jpg"
+	},{
+		id : 47,
+		name : "Guy Zombie",
+		img_src : "http://th05.deviantart.net/fs24/150/f/2007/352/3/8/mmm_zombie_by_Oberlain.jpg"
+	},{
+		id : 48,
+		name : "Ninja Turtle",
+		img_src : "http://th06.deviantart.net/fs47/150/i/2009/228/e/c/Little_ninja_turtles_by_Witchking00.jpg"
+	},{
+		id : 49,
+		name : "Link or Zelza",
+		img_src : "http://th03.deviantart.net/fs50/150/f/2009/339/b/4/princess_zelda_and_link_by_jeeba92.jpg"
+	},{
+		id : 50,
+		name : "Lara Croft",
+		img_src : "http://th09.deviantart.net/fs19/150/i/2007/246/7/b/Lara_Croft_by_ssava.jpg"
+	},{
+		id : 51,
+		name : "Katniss",
+		img_src : "http://th00.deviantart.net/fs70/150/i/2010/174/3/e/Brooding_Katniss_by_Ratgirlstudios.jpg"
+	},{
+		id : 52,
+		name : "Merida from Brave",
+		img_src : "http://th09.deviantart.net/fs71/150/f/2012/118/7/9/merida_by_flying_fox-d4xu2he.png"
+	},{
+		id : 53,
+		name : "Ghostbuster or Slimer",
+		img_src : "http://th02.deviantart.net/fs44/150/i/2009/059/7/5/The_fifth_Ghostbuster_by_savagelucy42.jpg"
+	},{
+		id : 54,
+		name : "Fry",
+		img_src : "http://th09.deviantart.net/fs70/150/f/2011/299/b/5/fry_by_rober_raik-d4e0fvb.png"
+	},{
+		id : 55,
+		name : "Bender",
+		img_src : "http://png-3.findicons.com/files//icons/1316/futurama_vol_1/128/bender.png"
+	},{
+		id : 56,
+		name : "Indiana Jones",
+		img_src : "http://th01.deviantart.net/fs70/150/i/2011/037/2/4/indiana_jones_by_imperviousbriarpatch-d3434cq.jpg"
+	},{
+		id : 57,
+		name : "Jack Sparrow",
+		img_src : "http://th07.deviantart.net/fs11/150/f/2011/047/c/9/c93623a3b05cbd6fb0f4dd47dd8a6588-dndyvk.jpg"
+	},{
+		id : 58,
+		name : "Leela",
+		img_src : "http://png-1.findicons.com/files//icons/1316/futurama_vol_1/128/leela.png"
+	},{
+		id : 59,
+		name : "Amy",
+		img_src : "http://png-5.findicons.com/files//icons/1316/futurama_vol_1/128/amy.png"
+	},{
+		id : 60,
+		name : "Why not Zoidberg",
+		img_src : "http://www.veryicon.com/icon/png/Movie%20%26%20TV/Futurama%20Vol.%201/Zoidberg.png"
+	},{
+		id : 61,
+		name : "Sailor Moon",
+		img_src : "http://upload.wikimedia.org/wikipedia/en/thumb/f/fc/Sailor_Moon_%281st_uniform%29.png/220px-Sailor_Moon_%281st_uniform%29.png"
+	},
+];
+
+var tmp = [
+	{
+		id : 0,
+		name : "",
+		img_src : ""
+	},
+];
