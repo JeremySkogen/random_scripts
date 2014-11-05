48ef99fcc10342ca88379e7c12c2e0871b8ea90c
jskogen <jeremyskogen@gmail.com>
2014-11-05 10:21:51 -0800

    updated

--- st/lightcrest_test.sh
+++ st/lightcrest_test.sh
@@ -7,30 +7,50 @@ if [[ $UID != 0 ]]; then
 fi
 
 # Check for backed up files
-if [ ! -f /etc/hosts_lc ]; then
+if [ ! -f /etc/hosts_lc_ext ]; then
   echo "First Run, creating backup..."
   cp /etc/hosts /etc/hosts_backup
   echo "Backup saved as /etc/hosts_backup"
 
   echo "Creating a new lighcrest hosts file...."
-  cp /etc/hosts /etc/hosts_lc
+  cp /etc/hosts /etc/hosts_lc_ext
 
-  cat >> /etc/hosts_lc <<EOL
+  cat >> /etc/hosts_lc_ext <<EOL
 
 # Version 1.0
 # Overrides for testing
 8.26.65.171 www.stocktwits.com
 8.26.65.171 stocktwits.com
+8.26.65.171 stocktwits.net
+
+
+
+8.26.65.171 assets0.new.stocktwits.net
+8.26.65.171 assets0.stocktwits.net
+8.26.65.171 assets0.new.stocktwits.com
+8.26.65.171 assets0.stocktwits.com
+8.26.65.171 assets1.new.stocktwits.net
+8.26.65.171 assets1.stocktwits.net
+8.26.65.171 assets1.new.stocktwits.com
+8.26.65.171 assets1.stocktwits.com
+
+8.26.65.171 assets2.new.stocktwits.net
+8.26.65.171 assets2.stocktwits.net
+8.26.65.171 assets2.new.stocktwits.com
+8.26.65.171 assets2.stocktwits.com
+
+8.26.65.171 assets3.new.stocktwits.net
+8.26.65.171 assets3.stocktwits.net
+8.26.65.171 assets3.new.stocktwits.com
+8.26.65.171 assets3.stocktwits.com
+
+8.26.65.171 new.stocktwits.net
+8.26.65.171 store.stocktwits.net
+8.26.65.171 new.stocktwits.com
+8.26.65.171 store.stocktwits.com
+
 
 8.26.65.173 api.stocktwits.com
-8.26.65.171 assets0.new.stocktwits.com
-8.26.65.171 assets0.stocktwits.net
-8.26.65.171 assets1.new.stocktwits.com
-8.26.65.171 assets1.stocktwits.net
-8.26.65.171 assets2.new.stocktwits.com
-8.26.65.171 assets2.stocktwits.net
-8.26.65.171 assets3.new.stocktwits.com
-8.26.65.171 assets3.stocktwits.net
 
 8.26.65.174 es.stocktwits.com
 
@@ -51,7 +71,7 @@ case "$1" in
   start)
     # Oracle listener and instance startup
     echo -n "Entering Testing mode...  "
-    cp /etc/hosts_lc /etc/hosts
+    cp /etc/hosts_lc_ext /etc/hosts
     echo "Begin testing"
     ;;
   stop)
@@ -65,11 +85,11 @@ case "$1" in
     echo -n "Removing residual files...  "
     cp /etc/hosts_backup /etc/hosts
     rm /etc/hosts_backup
-    rm /etc/hosts_lc
+    rm /etc/hosts_lc_ext
     echo "Hosts restored to original"
     ;;
   *)
-    echo "Usage: $0 start|stop"
+    echo "Usage: $0 start|stop|remove"
     exit 1
 esac
 
